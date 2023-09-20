4217114c30111eed6e91c678a36740720382b7c7
NealBCG <nealgr@gmail.com>
2023-09-19 21:37:22 -0400

    finishes index.php

--- assgn03/asgn03-static/index.php
+++ assgn03/asgn03-static/index.php
@@ -11,8 +11,10 @@
       include 'Bird.php';
       $bird = new Bird;
       echo '<p>The generic song of any bird is "' . $bird->song . '".</p>';
+
       $fly_catcher = new YellowBelliedFlyCatcher;
       echo '<p>The song of the ' . $fly_catcher->name . ' on breeding grounds is "' . $fly_catcher->song . '".</p>';
+
       $kiwi = new Kiwi;
       $kiwi->flying = "no";
       echo "<p>The " . $fly_catcher->name . " " . $fly_catcher->can_fly() . ".</p>";
@@ -22,9 +24,19 @@
     <h1>Static Examples</h1>
     <h2>Before using the create method</h2>
     <?php
+      echo "<p>Bird count: " . Bird::$instance_count . "</p>";
+      echo "<p>YellowBelliedFlyCatcher count: " . Bird::$instance_count . "</p>";
+      echo "<p>Kiwi count: " . Bird::$instance_count . "</p>";
     ?>
     <h2>After using the create method</h2>
     <?php
+      Bird::create();
+      YellowBelliedFlyCatcher::create();
+      Kiwi::create();
+
+      echo "<p>Bird count: " . Bird::$instance_count . "</p>";
+      echo "<p>YellowBelliedFlyCatcher count: " . Bird::$instance_count . "</p>";
+      echo "<p>Kiwi count: " . Bird::$instance_count . "</p>";
     ?>
   </body>
 </html>
