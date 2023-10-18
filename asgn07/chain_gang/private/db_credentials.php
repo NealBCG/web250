c2092f834ca185db7dbfc06f0fb0ee85b801081e
NealBCG <122643455+NealBCG@users.noreply.github.com>
2023-10-18 11:36:28 -0400

    Update db_credentials.php

--- asgn07/chain_gang/private/db_credentials.php
+++ asgn07/chain_gang/private/db_credentials.php
@@ -5,10 +5,4 @@ define("DB_USER", "webuser");
 define("DB_PASS", "secretpassword");
 define("DB_NAME", "chain_gang");
 
-
-//define("DB_SERVER", "localhost");
-//define("DB_USER", "utggeqphv5rbq");
-//define("DB_PASS", "ch@ing@ng4");
-//define("DB_NAME", "dbsgt5mvnqkgu7");
-
 ?>
