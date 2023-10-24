14dad8618bac90b3d0f3069a36c8490219f889d8
NealBCG <nealgr@gmail.com>
2023-10-24 16:33:21 -0400

    finishes code for localhost

---
+++ asgn08/active-birds/private/db_credentials.php
@@ -0,0 +1,14 @@
+<?php
+
+define("DB_SERVER", "localhost");
+define("DB_USER", "sabirdsUser");
+define("DB_PASS", "cassowary");
+define("DB_NAME", "sabirds");
+
+
+//define("DB_SERVER", "localhost");
+//define("DB_USER", "");
+//define("DB_PASS", "");
+//define("DB_NAME", "");
+
+?>
