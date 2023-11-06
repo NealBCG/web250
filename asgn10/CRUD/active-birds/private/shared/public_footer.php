d5fec3755a376b8a510c5c0d0e96faa1c42559db
NealBCG <nealgr@gmail.com>
2023-11-06 16:55:27 -0500

    setes up files for asgn10

---
+++ asgn10/CRUD/active-birds/private/shared/public_footer.php
@@ -0,0 +1,9 @@
+    
+  <footer>
+    <?php include(SHARED_PATH . '/public_copyright_disclaimer.php'); ?>
+  </footer>
+  
+  </body>
+</html>
+
+<?php db_disconnect($database); ?>
---
+++ asgn10/CRUD/active-birds/public/birds.php
@@ -0,0 +1,45 @@
+<?php 
+  require_once('../private/initialize.php');
+  $page_title = 'Sightings';
+  include(SHARED_PATH . '/public_header.php');
+?>
+
+<div id="main">
+  <div id="page">
+    <div class="intro">
+      <h2>Bird Inventory</h2>
+      <p>This is a short list -- start your birding!</p>
+    </div>
+
+    <div class="actions">
+      <a class="action" href="<?php echo url_for('/new.php'); ?>">Add Bird</a>
+    </div>
+
+    <table id="inventory" border=1>
+      <tr>
+        <th>Common Name</th>
+        <th>Habitat</th>
+        <th>Food</th>
+        <th>Conservation ID</th>
+        <th>Backyard Tips</th>
+      </tr>
+    
+      <?php $bird = Bird::find_all();
+      foreach($bird as $bird) { 
+           ?>
+        <tr>
+          <td><?php echo h($bird->common_name) ?></td>
+          <td><?php echo h($bird->habitat) ?></td>
+          <td><?php echo h($bird->food) ?></td>
+          <td><?php echo h($bird->conservation()) ?></td>
+          <td><?php echo h($bird->backyard_tips) ?></td>
+          <td><a href="detail.php?id=<?php echo $bird->id ?>">view</a></td>
+          <td><a href="detail.php?id=<?php echo $bird->id ?>">edit</a></td>
+          <td><a href="detail.php?id=<?php echo $bird->id ?>">delete</a></td>
+        </tr>
+      <?php } ?>
+    </table>
+  </div>
+</div>
+
+<?php include(SHARED_PATH . '/public_footer.php'); ?>
