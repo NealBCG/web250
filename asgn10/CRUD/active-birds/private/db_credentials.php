64c4a8f90d2fd8af9b0bf341635769f881717637
NealBCG <nealgr@gmail.com>
2023-11-07 22:33:08 -0500

    removes online credentials

--- asgn10/CRUD/active-birds/private/db_credentials.php
+++ asgn10/CRUD/active-birds/private/db_credentials.php
@@ -5,10 +5,4 @@ define("DB_USER", "sabirdsUser");
 define("DB_PASS", "cassowary");
 define("DB_NAME", "sabirds");
 
-
-//define("DB_SERVER", "localhost");
-//define("DB_USER", "ueqq5oawfuwod");
-//define("DB_PASS", "HRBJbXK9EScW88Y");
-//define("DB_NAME", "dbtkkfgixykqg0");
-
 ?>
