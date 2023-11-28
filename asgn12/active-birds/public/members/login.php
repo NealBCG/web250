df928e10098a5153037d1e0496a727919df6861d
NealBCG <nealgr@gmail.com>
2023-11-28 17:51:19 -0500

    implement captcha, code from https://www.the-art-of-web.com/php/captcha/

--- asgn12/active-birds/public/members/login.php
+++ asgn12/active-birds/public/members/login.php
@@ -20,6 +20,10 @@
     if(is_blank($password)) {
       $errors[] = "Password cannot be blank.";
     }
+    if($_POST['captcha'] != $_SESSION['digit']) {
+      die("Sorry, the CAPTCHA code entered was incorrect!");
+      session_destroy();
+    }
 
     // if there were no errors, try to login
     if(empty($errors)) {
@@ -50,8 +54,15 @@
   <form action="login.php" method="post">
     Username:<br>
     <input type="text" name="username" value="<?php echo h($username); ?>"><br>
+    <br>
     Password:<br>
     <input type="password" name="password" value=""><br>
+    <br>
+    Captcha:<br>
+    <img src="captcha.php" width="240" height="60" border=1 alt="CAPTCHA">
+    <p><input type="text" size="6" maxlength="5" name="captcha" value=""><br>
+    <small>copy the digits from the image into this box</small></p>
+
     <input type="submit" name="submit" value="Submit">
   </form>
 </div>
